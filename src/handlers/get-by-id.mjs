691cfbb662522fd48a6256e364b0c17ab0a932bc
Pedro Prieto <pedpral@gmail.com>
2025-03-28 12:00:48 +0100

    CORS

--- src/handlers/get-by-id.mjs
+++ src/handlers/get-by-id.mjs
@@ -1,8 +1,8 @@
 // Create clients and set shared const values outside of the handler.
 
 // Create a DocumentClient that represents the query to add an item
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
@@ -13,19 +13,21 @@ const tableName = process.env.SAMPLE_TABLE;
  * A simple example includes a HTTP get method to get one item by id from a DynamoDB table.
  */
 export const getByIdHandler = async (event) => {
-  if (event.httpMethod !== 'GET') {
-    throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
+  if (event.httpMethod !== "GET") {
+    throw new Error(
+      `getMethod only accept GET method, you tried: ${event.httpMethod}`,
+    );
   }
   // All log statements are written to CloudWatch
-  console.info('received:', event);
- 
+  console.info("received:", event);
+
   // Get id from pathParameters from APIGateway because of `/{id}` at template.yaml
   const id = event.pathParameters.id;
- 
+
   // Get the item from the table
   // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#get-property
   var params = {
-    TableName : tableName,
+    TableName: tableName,
     Key: { id: id },
   };
 
@@ -35,13 +37,20 @@ export const getByIdHandler = async (event) => {
   } catch (err) {
     console.log("Error", err);
   }
- 
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify(item)
+    body: JSON.stringify(item),
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET",
+    },
   };
- 
+
   // All log statements are written to CloudWatch
-  console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+  console.info(
+    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`,
+  );
   return response;
-}
+};
